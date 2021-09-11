cf4c95cf9525c6a99aad2a78158e25b959ebb006
mohammad <mohammadnv79@gmail.com>
2021-09-11 16:36:26 +0430

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Router>
         <Switch>
           <Route path="/" exact >
