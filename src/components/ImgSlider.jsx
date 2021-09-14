c95f1578b391f650fc63affed038b626e68f6782
mohammad <mohammadnv79@gmail.com>
2021-09-14 13:56:28 +0430

    viewer and imgSlider added

--- src/components/ImgSlider.jsx
+++ src/components/ImgSlider.jsx
@@ -15,52 +15,89 @@ const ImgSlider = () => {
   };
 
   return (
-      <div>
-        <Carousel {...settings}>
-            <Wrap>
-                <a>
-                    <img src="/images/slider-badging.jpg" alt/>
-                </a>
-            </Wrap>
-        </Carousel>
-      </div>
+    <div>
+      <Carousel {...settings}>
+        <Wrap>
+          <a>
+            <img src="/images/slider-badging.jpg" alt="" />
+          </a>
+        </Wrap>
+        <Wrap>
+          <a>
+            <img src="/images/slider-badag.jpg" alt="" />
+          </a>
+        </Wrap>
+        <Wrap>
+          <a>
+            <img src="/images/slider-scale.jpg" alt="" />
+          </a>
+        </Wrap>
+        <Wrap>
+          <a>
+            <img src="/images/slider-scales.jpg" alt="" />
+          </a>
+        </Wrap>
+      </Carousel>
+    </div>
   );
 };
 
 const Carousel = styled(Slider)`
-    margin-top: 20px;
+  margin-top: 20px;
 
-    & > button {
-        opacity: 0;
-        height: 100%;
-        width: 5vw;
-        z-index: 1;
+  & > button {
+    opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
 
-        &:hover{
-            opacity: 1;
-            transition:opacity 0.2 ease 0s;
-        }
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2 ease 0s;
     }
-    ul li button{
-        &:hover{
-            font-size: 10px;
-            color: rgb(150, 158, 171);
-        }
+  }
+  ul li button {
+    &:hover {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+  li.slick-active button:before {
+    color: white;
+  }
+  .slick-list {
+    overflow: initial;
+  }
+  .slick-prev {
+    left: -75px;
+  }
+  .slick-next {
+    right: -75px;
+  }
+`;
+const Wrap = styled.div`
+  border-radius: 4px;
+  cursor: pointer;
+  position: relative;
 
+  a {
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    cursor: pointer;
+    display: block;
+    position: relative;
+    padding: 4px;
+    img {
+      width: 100%;
+      height: 100%;
     }
-    li.slick-active button:before{
-        color: white;
+    &:hover {
+        padding:0;
+        border: 1px solid rgba(249, 249, 249, 0.8);
+        transition-duration: 0.3s;
     }
-    .slick-list {
-        overflow: initial;
-    }
-    .slick-prev {
-        left: -75px;
-    }
-    .slick-next {
-        right: -75px;
-    }
-
-`
+  }
+`;
 
 export default ImgSlider;
